f6a2bfe76438fc96f20124e9d2c821294ac8c571
Raul Geronimo <raul090700@gmai.com>
2023-03-25 18:34:22 -0600

    Academia

--- BD/Gimnasio.sql
+++ BD/Gimnasio.sql
@@ -42,42 +42,58 @@ CREATE TABLE Grupos(
     FOREIGN KEY(idActividad) REFERENCES Actividades(codigo) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-/* Valores de Prueba */
-INSERT INTO Deportista VALUES
-    (null, 'Raul Gabriel', 'Geronimo Herrera', NOW(), 'Mensual', 1, 'Wushu', 200, 2.1, 'Lunes, Martes y Jueves 1:00 PM a 4:00 PM');
-
-INSERT INTO Instructor VALUES
-    ('RCBA001364', 'Brian Alejandro', 'Rodriguez Cruz', 3500, 5513467985, 'Lunes, Miercoles y Viernes 3:00 PM a 6:00 PM');
-
-INSERT INTO Actividades VALUES
-    (null, 'RCBA001364', 'Wushu', 'Es un arte milenario que engloba todos los aspectos de la defensa personal, el manejo de las armas y una importante filosofía.', 'https://cdn-icons-png.flaticon.com/512/928/928573.png');
-
-INSERT INTO Grupos VALUES
-    (null, 1, 1, 'Lunes, Miercoles y Viernes de 2:00 PM a 4:00 PM');
-
 /* Conteo de Alumnos por Grupo */
-SELECT Actividades.Codigo, Actividades.Nombre, COUNT(Grupos.idDeportista) AS Cantidad_Deportistas, Actividades.imagen, Instructor.Nombre AS rfcInstructor
+SELECT 
+Actividades.Codigo, 
+Actividades.Nombre, 
+COUNT(Grupos.idDeportista) AS Cantidad_Deportistas, 
+Actividades.imagen, 
+Instructor.Nombre AS rfcInstructor
 FROM Actividades
+
 INNER JOIN Grupos
 ON Actividades.codigo = Grupos.idActividad
+
 INNER JOIN Instructor
 ON Actividades.rfcInstructor = Instructor.rfc
+
 GROUP BY(Actividades.Codigo)
 ORDER BY(Actividades.Nombre);
 
 /* ------------------------------------------------------------------ INNER JOIN ------------------------------------------------------------------ */
 /* Actividades */
-SELECT Actividades.codigo, Actividades.Nombre AS nombre, Instructor.Nombre AS rfcInstructor, Actividades.descripcion, Actividades.imagen
+SELECT 
+Actividades.codigo, 
+Actividades.Nombre AS nombre, 
+Instructor.Nombre AS rfcInstructor, 
+Actividades.descripcion, 
+Actividades.imagen
 FROM Actividades
 INNER JOIN Instructor
 ON Actividades.rfcInstructor = Instructor.rfc
 ORDER BY(Actividades.Nombre);
 
 /* Deportista */
-SELECT idDeportista, nombre, apellidos, tipoInscripcion, noActividades, nombreActividades, cuota, promocion, horario, DATE_FORMAT(fechaInscripcion, "%d / %b / %Y - %r") AS FechaInscripcion  FROM Deportista WHERE idDeportista = 1;
+SELECT 
+idDeportista, 
+nombre, 
+apellidos, 
+tipoInscripcion, 
+noActividades, 
+nombreActividades, 
+cuota, 
+promocion, 
+horario, 
+DATE_FORMAT(fechaInscripcion, "%d / %b / %Y - %r") AS FechaInscripcion 
+FROM Deportista 
+WHERE idDeportista = 1;
 
 /* grupos */
-SELECT Grupos.codigo, Deportista.nombre AS Deportista, Actividades.nombre AS Actividad, Grupos.horarios, Actividades.imagen 
+SELECT Grupos.codigo, 
+Deportista.nombre AS Deportista, 
+Actividades.nombre AS Actividad, 
+Grupos.horarios, 
+Actividades.imagen 
 FROM Grupos 
 INNER JOIN Deportista 
 ON Grupos.idDeportista = Deportista.idDeportista 
@@ -86,12 +102,12 @@ ON Grupos.idActividad = Actividades.codigo
 ORDER BY(Actividades.nombre);
 
 /* Instructor */
-SELECT rfc, nombre, apellidos, salario, CONCAT("(", LEFT(telefono, 3), ") ", MID(telefono, 4, 3), "-", MID(telefono, 7, 4)) AS telefono, horarios FROM Instructor ORDER BY nombre;
-
-/* Ejecutar el Proyecto */
-/* cd Servidor
-npm run build
-npm run dev
-
-cd appGimnasio
-ng serve --open */
+SELECT 
+rfc, 
+nombre, 
+apellidos, 
+salario, 
+CONCAT("(", LEFT(telefono, 3), ") ", MID(telefono, 4, 3), "-", MID(telefono, 7, 4)) AS telefono, 
+horarios 
+FROM Instructor 
+ORDER BY nombre;
